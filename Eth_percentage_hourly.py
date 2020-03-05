17546e70b72d26e10078323c99922ff02a6cacd2
Harrison Lane <harrisonrlane3@gmail.com>
2020-03-04 21:43:18 -0500

    update

--- Eth_percentage_hourly.py
+++ Eth_percentage_hourly.py
@@ -8,9 +8,10 @@ link = 'https://www.livecoinwatch.com/'
 
 r = requests.get(link)
 soup = BeautifulSoup(r.content, 'html.parser')
-#soup.find_all('table')
+#Quick tip, if you want to see all entries in livecoin just do print(df) and you'll see where the info gets pulled. 
 df = pandas.read_html(str(soup),header = 0)
 data = json.loads(df[0].to_json(orient = "records"))[1]
 while True:
-    print(data['1h'])
+    print("The current price rate of " + data['Coin'] +  " per hour " + data['1h'])
+    print("The current rate of " + data['Coin'] + " in the last 24 hours " + data['24h'])
     time.sleep(300)

dda2068124cff06024064b1035d04f0aa0c23a90
Harrison Lane <harrisonrlane3@gmail.com>
2020-03-10 17:55:07 -0400

    master

--- Eth_bot.py
+++ Eth_bot.py
@@ -1,3 +1,26 @@
 import discord
-from Eth_percentage_hourly import CoinWatch
-CoinWatch()
+import Eth_percentage_hourly
+import time
+d = Eth_percentage_hourly.Eth_price('https://www.livecoinwatch.com/')
+
+TOKEN = 'TOKEN'
+client = discord.Client()
+old_price = 0
+price = 0
+@client.event
+async def on_ready():
+    channel = client.get_channel(686390614862200841)
+    while True:
+        if old_price > price:
+            peakerwood = d.Etherium()
+            price = peakerwood[4]
+            await channel.send(" The currently hourly price of " + peakerwood[0] + " is " + peakerwood[4])
+            old_price = price
+        elif old_price < price:
+            await channel.send(" The currently hourly price of " + peakerwood[0] + " is " + peakerwood[4])
+        else:
+            return 0
+        time.sleep(1800)
+
+
+client.run(TOKEN)
--- Eth_percentage_hourly.py
+++ Eth_percentage_hourly.py
@@ -6,15 +6,21 @@ import time
 
 link = 'https://www.livecoinwatch.com/'
 
-def CoinWatch():
-    r = requests.get(link)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    #Quick tip, if you want to see all entries in livecoin just do print(df) and you'll see where the info gets pulled. 
-    df = pandas.read_html(str(soup),header = 0)
-    data = json.loads(df[0].to_json(orient = "records"))[1]
-    while True:
-        print("The current price rate of " + data['Coin'] +  " per hour " + data['1h'])
-        print("The current rate of " + data['Coin'] + " in the last 24 hours " + data['24h'])
-        time.sleep(10)
-        print("Current price of " + data['Liquidity ±2%'])
+class Eth_price():
+    def __init__(self, link):
+        self.shit = link
+    
+    def Etherium(self):
+        r = requests.get(self.shit)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        #Quick tip, if you want to see all entries in livecoin just do print(df) and you'll see where the info gets pulled. 
+        df = pandas.read_html(str(soup),header = 0)
+        data = json.loads(df[0].to_json(orient = "records"))[1]
+        # while True:
+        #     print("The current price rate of " + data['Coin'] +  " per hour " + data['1h'])
+        #     print("The current rate of " + data['Coin'] + " in the last 24 hours " + data['24h'])
+        #     time.sleep(10)
+        #     print("Current price of " + data['Liquidity ±2%'])
+        return(data['Coin'], data['1h'], data['24h'], data['Liquidity ±2%'], data['Price'])
+
 

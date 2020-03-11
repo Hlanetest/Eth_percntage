6bc63c28bf4e8e358347ba3a0abafe0a6d05bf47
Harrison Lane <harrisonrlane3@gmail.com>
2020-03-11 17:16:38 -0400

    master

--- Eth_bot.py
+++ Eth_bot.py
@@ -1,26 +1,51 @@
 import discord
 import Eth_percentage_hourly
 import time
+
 d = Eth_percentage_hourly.Eth_price('https://www.livecoinwatch.com/')
 
-TOKEN = 'TOKEN'
+TOKEN = 'Njg1NTU4NTI5NDU2ODY1Mjgw.XmjmzQ.KmdaIYkai2mvJAUZ4ENAAaYKj6I'
 client = discord.Client()
-old_price = 0
-price = 0
 @client.event
 async def on_ready():
+    price = int(0)
+    print('1')
+    old_price = float(0)
+    print('2')
     channel = client.get_channel(686390614862200841)
+    print('3')
     while True:
-        if old_price > price:
-            peakerwood = d.Etherium()
-            price = peakerwood[4]
-            await channel.send(" The currently hourly price of " + peakerwood[0] + " is " + peakerwood[4])
-            old_price = price
-        elif old_price < price:
-            await channel.send(" The currently hourly price of " + peakerwood[0] + " is " + peakerwood[4])
+        print('Now we loop!')
+        peakerwood = d.Etherium()
+        price = float(peakerwood[4].replace('$', ''))
+        per = int(0)
+        if price >= old_price:
+            print('I am here again')
+            if old_price == 0:
+                await channel.send("The current price of " + peakerwood[0] + " is " + peakerwood[4])
+                print('Ima loop')
+                old_price = price
+                print('I got dat $$$')
+            elif price == old_price:
+                await channel.send("The current price of " + peakerwood[0]+ " Has remained the same at " + peakerwood[4])
+                old_price = price
+
+            else:
+                per = int(price - old_price)
+                per_total = float(per / old_price * 100)
+                await channel.send(" The currently hourly price of "+ peakerwood[0]+" has gone up to "+ peakerwood[4]+ ' '  + per_total)
+                old_price = price
+            
+            
+        elif price < old_price:
+            per = int(old_price - price)
+            print (price)
+            per_total = float(per / old_price * 100)
+            print(old_price)
+            await channel.send("The currently hourly price of " + peakerwood[0] + " has gone down to " + peakerwood[4] + " It has decreased to " + per_total)
         else:
             return 0
-        time.sleep(1800)
+        time.sleep(300)
 
 
 client.run(TOKEN)
